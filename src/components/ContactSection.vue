c66bdb0cf770bc09929495a071177f20844dc264
AKCIZUR <ruzickajakub95@gmail.com>
2026-06-11 16:14:52 +0000

    fj

--- src/components/ContactSection.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section id="kontakt" class="contact" data-motion-section>
-    <div class="container">
-      <div class="section-label" data-motion="fade">POJĎME BUILDIT</div>
-      <h2 class="section-title" data-motion="slide-up">MÁTE<br />PROJEKT?</h2>
-      <a href="mailto:mail@ruzickajakub.cz" class="contact-email" data-motion="fade">mail@ruzickajakub.cz</a>
-    </div>
-  </section>
-</template>
